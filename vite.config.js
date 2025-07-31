eda2f0f3727c589c173341fe581a7c7c09086632
BasdB97 <113598535+BasdB97@users.noreply.github.com>
2025-07-31 15:17:21 +0200

    Installed Tailwind CSS

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,13 @@
 import legacy from "@vitejs/plugin-legacy";
 import vue from "@vitejs/plugin-vue";
 import path from "path";
+import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	base: "/",
-	plugins: [vue(), legacy()],
+	plugins: [vue(), legacy(), tailwindcss()],
 	resolve: {
 		alias: {
 			"@": path.resolve(__dirname, "./src"),
